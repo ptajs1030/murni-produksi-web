ac7266f72a0d31ab88d4a0ed34450157a60951c7
N0Xzyzy <nosssssrtrakzzz@gmail.com>
2026-01-27 15:55:29 +0700

    Pembuatan dashboard

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', \App\Http\Controllers\Api\LoginController::class);
+Route::middleware('auth')->group(function () {
+    Route::middleware('admin, owner')->group(function () {
+
+    });
+
+});
