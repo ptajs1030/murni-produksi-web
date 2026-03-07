975414b87ed54a60549e195af11b1a2af1caa1b0
NaufalDanadyaksa <naufal060906@gmail.com>
2026-03-07 10:40:55 +0700

    bug fixing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,11 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-         $middleware->alias([
+        $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
-        ]);
-        $middleware->web(append:[
-            HandleInertiaRequests::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
