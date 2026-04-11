16df4419369e2f9c9d75f7b5331cb8b9214911df
muhamadsaid <msaida97@gmail.com>
2026-04-11 08:53:04 +0700

    fix api repack

--- app/Service/RepackService.php
+++ app/Service/RepackService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Models\CoreProduct;
 use App\Models\CoreStock;
 use App\Models\CoreStockTransaction;
 use Illuminate\Support\Facades\DB;
@@ -11,7 +12,21 @@ class RepackService
 {
     public function getRepack(?string $search = null): array
     {
-        $repack = CoreStockTransaction::query()
+
+        $query = CoreProduct::where("product_type", "Produk Bahan Baku")
+            ->with(['stocks', 'packagingSize']);
+
+        if ($search !=null) {
+            $search = $search;
+            $query->where(function ($q) use ($search) {
+                $q->where('product_unit_sku', 'like', "%{$search}%")
+                    ->orWhere('product_name', 'like', "%{$search}%");
+            });
+        }
+
+        $products = $query->orderBy('product_name')->paginate(15);
+
+        $repack = /*CoreStockTransaction::query()
             ->select([
                 'id',
                 'product_id',
@@ -40,16 +55,28 @@ class RepackService
                         $q->where('name', 'like', "%{$search}%");
                     });
                 });
-            })
+            })*/
+      CoreProduct::where("product_type", "Produk Bahan Baku")
+            ->with(['stocks', 'packagingSize'])
+            ->select('id', 'product_name', 'product_unit_sku', 'm_packaging_size_id')
             ->orderByDesc('created_at')
             ->paginate(10);
+       /* $results = $repack->map(function ($item) {
+            return [
+                'id'             => $item->product_id,
+                'productName'   => $item->product->product_name ?? '-',
+                'packagingSize' => $item->product->packagingSize->packaging_size_name ?? '-',
+                'productQty'      => $item->packaging_size_input ?? 0,
+                'productUnitSku' => $item->product->product_unit_sku ?? '-',
+            ];
+        });*/
 
         return [
-            'data' => $repack->items(),
+            'data' => $products,
             'meta' => [
-                'current_page' => $repack->currentPage(),
-                'last_page'    => $repack->lastPage(),
-                'total'        => $repack->total(),
+                'current_page' => $products->currentPage(),
+                'last_page'    => $products->lastPage(),
+                'total'        => $products->total(),
             ]
         ];
     }
